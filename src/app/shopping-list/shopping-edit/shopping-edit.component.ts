ac85c5a523bab1e72cdab4335ee751098eeaa6fd
gaureshgawasmn <gaureshgawasmn@gmail.com>
2024-12-04 22:03:39 +0530

    added reactive form in shopping edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,23 +1,49 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime } from 'rxjs';
 import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
-  name: string = '';
-  amount: number = 0;
   private shoppingListService = inject(ShoppingListService);
+  shoppingItemForm: FormGroup = new FormGroup({
+    name: new FormControl<string>('', {
+      validators: [Validators.required, Validators.minLength(3)],
+    }),
+    amount: new FormControl<number | null>(null, {
+      validators: [Validators.required, Validators.min(1)],
+    }),
+  });
+
+  ngOnInit() {
+    this.shoppingItemForm.valueChanges.pipe(debounceTime(500)).subscribe(() => {
+      console.log(this.shoppingItemForm.value);
+    });
+  }
 
   addIngredient() {
+    if (this.shoppingItemForm.invalid) {
+      return;
+    }
     this.shoppingListService.addIngredient({
-      name: this.name,
-      amount: this.amount,
+      name: this.shoppingItemForm.get('name')?.value,
+      amount: this.shoppingItemForm.get('amount')?.value,
     });
+    this.reset();
+  }
+
+  reset() {
+    this.shoppingItemForm.reset();
   }
 }
